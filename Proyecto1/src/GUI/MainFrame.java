247c52debafb1048309acd1c8c702ad96504048c
Jose Manuel Martinez Martinez <manny.ito@zoho.com>
2018-05-11 11:36:19 +0000

    Interfaz para los nodos

--- Proyecto1/src/GUI/MainFrame.java
+++ Proyecto1/src/GUI/MainFrame.java
@@ -24,8 +24,8 @@ import javax.swing.JSpinner;
 public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtFf;
 	private JTable table;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -48,78 +48,102 @@ public class MainFrame extends JFrame {
 	 */
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 488, 347);
+		setBounds(100, 100, 488, 315);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(0, 0, 482, 315);
+		tabbedPane.setBounds(0, 0, 482, 284);
 		contentPane.add(tabbedPane);
 		
-		JPanel panel = new JPanel();
-		tabbedPane.addTab("Inicio", null, panel, null);
-		panel.setLayout(null);
+		JPanel servidores = new JPanel();
+		tabbedPane.addTab("Inicio", null, servidores, null);
+		servidores.setLayout(null);
 		
-		txtFf = new JTextField();
-		txtFf.setBounds(31, 12, 266, 19);
-		panel.add(txtFf);
-		txtFf.setColumns(10);
+		JLabel lblNodo = new JLabel("Nodo");
+		lblNodo.setBounds(12, 12, 48, 15);
+		servidores.add(lblNodo);
 		
-		JButton btnAbrir = new JButton("Abrir");
-		btnAbrir.setBounds(310, 9, 117, 25);
-		panel.add(btnAbrir);
+		textField = new JTextField();
+		textField.setBounds(66, 10, 307, 19);
+		servidores.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnAadir_2 = new JButton("A\u00F1adir");
+		btnAadir_2.setBounds(385, 7, 80, 25);
+		servidores.add(btnAadir_2);
+		
+		JList list_1 = new JList();
+		list_1.setBounds(12, 38, 453, 177);
+		servidores.add(list_1);
+		
+		JButton btnEditar = new JButton("Editar");
+		btnEditar.setBounds(12, 220, 80, 25);
+		servidores.add(btnEditar);
+		
+		JButton btnEliminar_1 = new JButton("Eliminar");
+		btnEliminar_1.setBounds(104, 220, 104, 25);
+		servidores.add(btnEliminar_1);
+		
+		JButton btnAceptar_1 = new JButton("Aceptar");
+		btnAceptar_1.setBounds(376, 220, 89, 25);
+		servidores.add(btnAceptar_1);
+		
+		JPanel operaciones = new JPanel();
+		tabbedPane.addTab("Inicio", null, operaciones, null);
+		operaciones.setLayout(null);
 		
 		JLabel lblOperaciones = new JLabel("Operaciones");
-		lblOperaciones.setBounds(12, 42, 107, 15);
-		panel.add(lblOperaciones);
+		lblOperaciones.setBounds(12, 12, 107, 15);
+		operaciones.add(lblOperaciones);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Resta", "Suma", "Multiplicacion", "Division"}));
-		comboBox.setBounds(22, 61, 132, 24);
-		panel.add(comboBox);
+		comboBox.setBounds(22, 31, 132, 24);
+		operaciones.add(comboBox);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Pares"}));
-		comboBox_1.setBounds(22, 215, 132, 24);
-		panel.add(comboBox_1);
+		comboBox_1.setBounds(22, 185, 132, 24);
+		operaciones.add(comboBox_1);
 		
 		JLabel lblFiltros = new JLabel("Restricciones");
-		lblFiltros.setBounds(12, 188, 107, 15);
-		panel.add(lblFiltros);
+		lblFiltros.setBounds(12, 158, 107, 15);
+		operaciones.add(lblFiltros);
 		
 		JButton btnAadir = new JButton("A\u00F1adir");
-		btnAadir.setBounds(31, 251, 117, 25);
-		panel.add(btnAadir);
+		btnAadir.setBounds(31, 221, 117, 25);
+		operaciones.add(btnAadir);
 		
 		JList list = new JList();
-		list.setBounds(173, 43, 292, 160);
-		panel.add(list);
+		list.setBounds(173, 13, 292, 160);
+		operaciones.add(list);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setBounds(84, 90, 70, 20);
-		panel.add(spinner);
+		spinner.setBounds(84, 60, 70, 20);
+		operaciones.add(spinner);
 		
 		JButton btnSubir = new JButton("subir");
-		btnSubir.setBounds(182, 214, 70, 25);
-		panel.add(btnSubir);
+		btnSubir.setBounds(182, 184, 70, 25);
+		operaciones.add(btnSubir);
 		
 		JButton btnBajar = new JButton("bajar");
-		btnBajar.setBounds(256, 214, 88, 25);
-		panel.add(btnBajar);
+		btnBajar.setBounds(256, 184, 88, 25);
+		operaciones.add(btnBajar);
 		
 		JButton btnEliminar = new JButton("Eliminar");
-		btnEliminar.setBounds(356, 214, 97, 25);
-		panel.add(btnEliminar);
+		btnEliminar.setBounds(356, 184, 97, 25);
+		operaciones.add(btnEliminar);
 		
 		JButton btnAadir_1 = new JButton("A\u00F1adir");
-		btnAadir_1.setBounds(31, 111, 117, 25);
-		panel.add(btnAadir_1);
+		btnAadir_1.setBounds(31, 81, 117, 25);
+		operaciones.add(btnAadir_1);
 		
 		JButton btnAceptar = new JButton("Aceptar");
-		btnAceptar.setBounds(336, 251, 117, 25);
-		panel.add(btnAceptar);
+		btnAceptar.setBounds(336, 221, 117, 25);
+		operaciones.add(btnAceptar);
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Resultados", null, panel_1, null);
